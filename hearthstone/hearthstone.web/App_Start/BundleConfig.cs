7049d7670a4781b5ade2c950f756502c43103d94
franzPilgerstorfer <franz.pilgerstorfer@pundt.at>
2017-04-28 09:40:19 +0200

    modified template added first cool stuff

--- hearthstone/hearthstone.web/App_Start/BundleConfig.cs
+++ hearthstone/hearthstone.web/App_Start/BundleConfig.cs
@@ -9,23 +9,24 @@ namespace hearthstone.web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/content/scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/content/scripts/jquery.validate*"));
 
             // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
             // für die Produktion bereit sind, verwenden Sie das Buildtool unter "http://modernizr.com", um nur die benötigten Tests auszuwählen.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/content/scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/content/scripts/bootstrap.js",
+                      "~/content/scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/content/styles/bootstrap.css",
+                "~/content/styles/site.css"
+                      ));
         }
     }
 }
